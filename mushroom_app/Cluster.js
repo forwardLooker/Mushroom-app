ca92ae77921b297f37649e075cb1793092033762
Anvar_Khakimov <pater@spark-mail.ru>
2026-03-14 00:36:36 +0300

    First commit

---
+++ mushroom_app/Cluster.js
@@ -0,0 +1,112 @@
+const fs = require('fs');
+
+module.exports = class Cluster {
+
+  id;
+  N = 0;
+  static sumN = 0;
+  
+  S = 0;
+  W = 0;
+  Occ = {};
+  
+  static r = 2.6;
+  static TRANSACTION_LENGTH = 23;
+
+  static clusters = [];
+
+  static transactionIndexes = {};
+
+  constructor({id}) {
+    this.id = id;
+  }
+
+  static calcProfitForNewCluster(tr) {
+    let sumGradientFromOtherClusters = 0;
+    Cluster.clusters.forEach(cl => {
+      sumGradientFromOtherClusters = sumGradientFromOtherClusters + cl.getGradient();
+    });
+
+    const profit = ((sumGradientFromOtherClusters + (Cluster.TRANSACTION_LENGTH / (Math.pow(Cluster.TRANSACTION_LENGTH, Cluster.r) )))) / Cluster.sumN;
+    return profit;
+
+  }
+
+  calcProfit(tr) {
+    const profit = ((this.sumGradientFromOtherClusters + this.getGradient(tr))) / Cluster.sumN;
+    return profit;
+  }
+
+  get sumGradientFromOtherClusters() {
+    const otherClusters = Cluster.clusters.filter(cl => cl.id !== this.id);
+    let sumGradient = 0;
+    otherClusters.forEach(cl => {
+      sumGradient = sumGradient + cl.getGradient();
+    });
+    return sumGradient;
+  };
+
+  getGradient(tr) {
+    if (!tr) {
+      const gradient = (this.S / (Math.pow(this.W, Cluster.r))) * this.N;
+      return gradient;
+    } else {
+      const newS = this.S + Cluster.TRANSACTION_LENGTH;
+      let newOcc = {...this.Occ};
+
+      const trArr = tr.split(',');
+      trArr.forEach((o, idx) => {
+        if (newOcc[idx + o]) {
+          newOcc[idx + o] = newOcc[idx + o] + 1;
+        } else {
+          newOcc[idx + o] = 1;
+        }
+      });
+      const newW = Object.keys(newOcc).length;
+
+      const gradient = (newS / (Math.pow(newW, Cluster.r))) * (this.N + 1);
+      return gradient;
+    }
+  }
+  
+  addTransaction(tr, trIndex) {
+    try {
+      if (this.N === 0) {
+        fs.writeFileSync(`cluster${this.id}.data`, tr);
+        this.N++;
+        this.S = this.N * Cluster.TRANSACTION_LENGTH;
+
+        const trArr = tr.split(',');
+        trArr.forEach((o, idx) => {
+          this.Occ[idx + o] = 1;
+        });
+        this.W = Object.keys(this.Occ).length;
+
+        Cluster.sumN++;
+
+        Cluster.transactionIndexes[trIndex] = this;
+      } else {
+        fs.appendFileSync(`cluster${this.id}.data`, '\r\n' + tr);
+        this.N++;
+        this.S = this.N * Cluster.TRANSACTION_LENGTH;
+
+        const trArr = tr.split(',');
+        trArr.forEach((o, idx) => {
+          if (this.Occ[idx + o]) {
+            this.Occ[idx + o] = this.Occ[idx + o] + 1;
+          } else {
+            this.Occ[idx + o] = 1;
+          }
+        });
+        this.W = Object.keys(this.Occ).length;
+
+        Cluster.sumN++;
+
+        Cluster.transactionIndexes[trIndex] = this;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+
+  }
+}
---
+++ mushroom_app/index.js
@@ -0,0 +1,57 @@
+const Cluster = require('./Cluster.js');
+const fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: fs.createReadStream('agaricus-lepiota.data'),
+  crlfDelay: Infinity // Treats '\r\n' as a single newline
+});
+
+let lineIdx = 0;
+
+rl.on('line', (line) => {
+  // console.log(`Line from file: ${line}`);
+  lineIdx++;
+  // console.log(`lineIdx from file: ${lineIdx}`);
+
+  if (Cluster.clusters.length === 0) {
+    const cluster = new Cluster({id: 1});
+    cluster.addTransaction(line, lineIdx);
+    Cluster.clusters.push(cluster);
+
+
+  } else {
+    const clusterProfitsArr = Cluster.clusters.map(cluster => {
+      const profit = cluster.calcProfit(line);
+      return {cluster, profit}
+    });
+    let profitMax = 0;
+    let clusterWithMaxProfit;
+
+    clusterProfitsArr.forEach(clObj => {
+      if (clObj.profit > profitMax) {
+        profitMax = clObj.profit;
+        clusterWithMaxProfit = clObj.cluster;
+      }
+    });
+
+    const profitForNewCluster = Cluster.calcProfitForNewCluster(line);
+
+    // console.log('profitForNewCluster', profitForNewCluster);
+    // console.log('profitMax', profitMax);
+
+    if (profitForNewCluster > profitMax) {
+      const cluster = new Cluster({id: Cluster.clusters.length + 1});
+      cluster.addTransaction(line, lineIdx);
+      Cluster.clusters.push(cluster);
+    } else {
+      clusterWithMaxProfit.addTransaction(line, lineIdx);
+    }
+  }
+  
+});
+
+
+lineIdx = 0;
+
+
+// Cluster 
